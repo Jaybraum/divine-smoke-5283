d15c3e39740e28690119d76cae9d7b8399e1fc54
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-14 19:19:02 +0200

    Adds plot order test

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -11,8 +11,6 @@ RSpec.describe Garden do
     @plot_1 = @garden.plots.create!(number: 101, size: "Large", direction:'North')
     @plot_2 = @garden.plots.create!(number: 102, size: "Small", direction:'East')
     @plot_3 = @garden.plots.create!(number: 103, size: "Medium", direction:'West')
-    @plot_4 = @garden.plots.create!(number: 11, size: "Large", direction:'North')
-    @plot_5 = @garden.plots.create!(number: 12, size: "Small", direction:'East')
 
     @plant_1 = Plant.create!(name: 'Rose', description: "loves the dirt", days_to_harvest: 120)
     @plant_2 = Plant.create!(name: 'Dandelion', description: "loves sunlight", days_to_harvest: 80)
@@ -25,9 +23,9 @@ RSpec.describe Garden do
     Planted.create!(plot:@plot_1, plant: @plant_1)
     Planted.create!(plot:@plot_1, plant: @plant_2)
     Planted.create!(plot:@plot_2, plant: @plant_3)
-    Planted.create!(plot:@plot_3, plant: @plant_4)
-    Planted.create!(plot:@plot_4, plant: @plant_5)
-    Planted.create!(plot:@plot_5, plant: @plant_6)
+    Planted.create!(plot:@plot_2, plant: @plant_4)
+    Planted.create!(plot:@plot_2, plant: @plant_5)
+    Planted.create!(plot:@plot_3, plant: @plant_6)
   end
 
   describe 'instance methods' do
@@ -42,6 +40,13 @@ RSpec.describe Garden do
     it 'does not include plants with harvest dates larger than 100' do
 
       expect(@garden.short_harvest).to_not include(@plant_3)
+    end
+
+    xit 'sorts plants by number of plants a plot has in descending order' do
+
+      expect(@garden.short_harvest[0].name).to eq(@plant_4)#because 3 plants in plot 2
+      expect(@garden.short_harvest[1].name).to eq(@plant_5)#because 3 plants in plot 2
+      expect(@garden.short_harvest[2].name).to eq(@plant_2)#because 2 plants in plot 1
     end
   end
 end
